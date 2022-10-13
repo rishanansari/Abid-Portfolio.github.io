ba941fa8ec766e7da34bcd99278aa994af77315f
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2022-10-13 16:16:22 +0530

    Update WorkExp.jsx

--- src/component/WorkExp.jsx
+++ src/component/WorkExp.jsx
@@ -14,7 +14,7 @@ const WorkExp = () => {
   <img src={relaible} alt="relaible info" style={{width:"20%",height:"10%"}}/>
   <div>
   <h2>Relaible Infosys</h2>
-  <p> Dec-2020 to Jan-2022</p>
+  <p> June-2021 to Sept-2022</p>
   <p>1+ Years of Experience as a Software Engineer at Reliable Infosys Thane. Worked 
 on HTML5, CSS, JavaScript and React-Js.
 </p>
@@ -24,7 +24,7 @@ on HTML5, CSS, JavaScript and React-Js.
   <img src={theem} alt="relaible info" style={{width:"20%",height:"10%"}}/>
   <div>
   <h2>Theem College of Engineering</h2>
-  <p> March-2017 to Dec-2021</p>
+  <p> March-2017 to June-2021</p>
   <p>4+ Years of Experience as an Assistance Professor at Theem College of Engineering,Boisar.
 </p>
 </div>
